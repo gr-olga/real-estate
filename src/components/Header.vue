0410c74b17a56a32d4d3c1f73cdfb9ed240a50a3
Olga Panfilova <gr-olga@ya.ru>
2022-11-19 14:27:24 +0100

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header">
+    Header
+    <img class="header__img" src="@/assets/images/img_logo_dtt@3x.png" alt="DTT">
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/_colors.scss' as color;
+
+.header {
+  color: color.$primary-element-color;
+
+  &__img {
+
+  }
+}
+
+</style>
