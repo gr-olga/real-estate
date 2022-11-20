bd8dce35b11906638f8c06db4fa0ac047440ea5a
Olga Panfilova <gr-olga@ya.ru>
2022-11-20 10:04:46 +0100

    added css to about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,72 @@
 </script>
 
 <template>
-  <div>
-    <h1> About DTT real estate</h1>
-    <p>kes rbgaelr na/lehga:ELdmg/.dznfb/lzdjg bak/jergbleNG `,MDF BM`DB GK;A EBRGKJAEN FBV,M`D B</p>
-    <H2>DESIGN AND DEVELOPMENT</H2>
-    <img src="@/assets/images/img_logo_dtt@3x.png" alt="logo">
-    <a>DTT</a>
+  <div class="about">
+    <h1 class="about__title"> About DTT real estate</h1>
+    <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h2 class="about__title">DESIGN AND DEVELOPMENT</h2>
+    <div class="about__addition">
+      <img src="@/assets/images/img_logo_dtt@3x.png" alt="logo" class="about__logo">
+      <div class="about__link">
+        <span class="about__link-title"> By DTT</span>
+        <a class="about__link-url">www.d-tt.nl</a>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.about {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  color: #4A4B4C;
+  max-width: 80%;
+  padding-bottom: 40px;
 
+  &__title {
+    font-size: 32px;
+    font-weight: bold;
+    margin-top: 15px;
+    color: black;
+  }
+
+  &__paragraph {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 18px;
+    margin-top: 15px;
+    margin-bottom: 40px;
+  }
+
+  &__addition {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  &__logo {
+    width: 300px;
+    margin-top: 15px;
+  }
+
+  &__link {
+    display: flex;
+    flex-direction: column;
+    margin-left: 25px;
+  }
+
+  &__link-url {
+    color: #067BC2;
+  }
+
+  &__link-title {
+    color: #4A4B4C;
+  }
+}
 </style>
