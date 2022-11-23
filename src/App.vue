87c4176a6a164f66d39f4ad970dd2bead5f48337
Olga Panfilova <gr-olga@ya.ru>
2022-11-23 12:27:33 +0100

    cleean up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router'
+import {RouterView} from 'vue-router'
 import Header from "@/components/Header.vue";
 import Popup from "@/components/Popup.vue";
-import {store} from "@/store";
-
-</script>
+import {store} from "@/store";</script>
 
 <template>
   <header>
     <Header/>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">overview</RouterLink>
-        <p></p>
-        <RouterLink to="/house-create">house-create</RouterLink>
-        <p></p>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
   <div class="app">
-    <RouterView/>
+  <RouterView/>
   </div>
   <Popup v-if="store.state.isPopupOpen"/>
 </template>
@@ -35,5 +24,10 @@ import {store} from "@/store";
   align-items: center;
   background-color: color.$background1-element-color;
   width: 100%;
+}
+
+.header {
+  padding-top: 20px;
+  padding-bottom: 20px;
 }
 </style>
