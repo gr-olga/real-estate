74dfa913a61d9eb2f4cafc102ecb4f324d281975
Olga Panfilova <gr-olga@ya.ru>
2022-11-23 12:50:36 +0100

    fixes for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,13 +4,17 @@
 
 <template>
   <div class="about">
-    <h1 class="about__title"> About DTT real estate</h1>
+    <h1 class="about__title">About DTT real estate</h1>
     <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
       dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h2 class="about__title">DESIGN AND DEVELOPMENT</h2>
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <h2 class="about__title">Design and Development</h2>
     <div class="about__addition">
       <img src="@/assets/images/img_logo_dtt@3x.png" alt="logo" class="about__logo">
       <div class="about__link">
@@ -22,13 +26,15 @@
 </template>
 
 <style scoped lang="scss">
+@use '@/assets/_colors.scss' as color;
+
 .about {
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  color: #4A4B4C;
+  color: color.$secondary-text-color;
   max-width: 80%;
   padding-bottom: 40px;
 
@@ -43,13 +49,13 @@
     font-family: 'Open Sans', sans-serif;
     font-size: 18px;
     margin-top: 15px;
-    margin-bottom: 40px;
+    margin-bottom: 20px;
   }
 
   &__addition {
     display: flex;
     flex-direction: row;
-    align-items: center;
+    align-items: flex-end;
   }
 
   &__logo {
@@ -64,11 +70,11 @@
   }
 
   &__link-url {
-    color: #067BC2;
+    color: color.$hyperlink-text-color;
   }
 
   &__link-title {
-    color: #4A4B4C;
+    color: color.$secondary-text-color;
   }
 }
 </style>
